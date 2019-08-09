1fdeb3db747103f638945cc0e6725453de493cce
victoria_kravets <victoriakravets93@gmail.com>
2019-08-09 12:18:31 +0300

    create project

---
+++ WeatherApp/Views/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Victoria on 8/9/19.
+//  Copyright © 2019 Victoria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
